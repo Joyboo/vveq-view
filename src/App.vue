41bb39e19bf04872c743d328407fd850cb0d26fa
Joyboo <Joyboo@aliyun.com>
2018-12-01 21:09:09 +0800

    新增ElementUI 完成响应式容器布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside style="background: inherit;" width="200px" class="hidden-sm-only hidden-xs-only"></el-aside>
+        <el-aside width="200px" class="hidden-sm-only hidden-xs-only">Aside</el-aside>
+        <el-main>Main</el-main>
+        <el-aside width="200px" class="hidden-sm-only hidden-xs-only">Aside</el-aside>
+        <el-aside style="background: inherit;" width="200px" class="hidden-sm-only hidden-xs-only"></el-aside>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import HelloWorld from './components/HelloWorld.vue'
+  import 'element-ui/lib/theme-chalk/display.css'
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
+
+  .el-header, .el-footer {
+    background-color: #515a6e;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+    margin: 10px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 1000px;
+    margin: 10px;
+  }
+
+  body > .el-container {
+    width: 100%;
+    margin-bottom: 40px;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
